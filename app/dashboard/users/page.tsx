45c8c301bb9b9bd521b6568348363f3682657d42
niravpandey2005 <36653269+niravpandey@users.noreply.github.com>
2025-11-16 07:21:55 +1100

    Basic React Routing

---
+++ app/dashboard/users/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Users = () => {
+    return (
+        <div>
+            <h1>Users</h1>
+
+            <ul className="mt-10">
+                <li>User 1</li>
+                <li>User 2</li>
+                <li>User 3</li>
+                <li>User 4</li>
+            </ul>
+        </div>
+    )
+}
+export default Users
